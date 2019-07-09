3051dc193d1dd805202d9e401ce5dc25de8647f9
Flor Aguilar <flor@MacBook-Air-de-Flor.local>
2019-07-09 09:59:04 -0500

    primer commit

---
+++ CódigoDePráctica/CódigoPython/Flor.py
@@ -0,0 +1,8 @@
+name = "Flor"
+age = 27 
+
+#Concatenate
+
+print("Hello my name is "+ name + "and I am " + str(age))
+
+print("My name is (name) and i am (age)".format(name="Flor", age=31)
---
+++ CódigoDePráctica/CódigoPython/Hello.py
@@ -0,0 +1,22 @@
+
+#Mi primer programa: Hola mundo!
+#print function
+
+print("Hello world")
+x = 1 
+y= 2.5
+name= "Flor"
+is_cool = True
+
+a = x+y 
+print(x, y, name, is_cool, a)
+#check type x 
+print(type(x))
+
+#cast, e.g., x to string
+
+x= str(x) #passing x 
+y= int(y) #passing y 
+z= float(y) 
+print(type(y), y)
+print(type(z), z)
